f0637b95850763e9bf10ed1980cdf71851e8c925
msh1n0 <Mb8awAPuYTmZ>
2014-04-11 15:48:28 +0200

    Erweiterung

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 include 'framework/framework.php';
 
 $framework = new framework();
-$framework->externalcontent->setURL('http://v8wsp/testarea/soschniok/hannover96vereint/ems.registration.club.php?l=de&xid=g32dpp37t3mcbq40v74hdloq06&s=manageclub&s=teams');
-$framework->externalcontent->cropContent('<h1>Mannschaften</h1>',true,'</table>', true);
-$framework->externalcontent->localise();
-$content=$framework->externalcontent->getContent();
+$content='';
 
 $framework->template->setTemplate('custom');
 $framework->template->setTemplateVariables(array('content',$content));
