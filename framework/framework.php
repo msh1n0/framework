55f86010943f8940b4b8d579925b7496d62d8ac0
msh1n0 <Mb8awAPuYTmZ>
2014-04-11 17:18:13 +0200

    Erweiterung

--- framework/framework.php
+++ framework/framework.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include 'configuration.inc';
 include 'contentpool.inc';
 include 'document.inc';
 include 'exceptions.inc';
@@ -11,11 +12,13 @@ include 'template.inc';
 include 'users.inc';
 
 
+global $CONFIG;
+
 /**
  * Class framework Sammlung Funktionen
  *
  * TODO Datenbank (einsetzen in document): Datenbank auswählen, auslesen, schreiben, bearbeiten, tabellen anlegen, tabellen löschen, Import, Export
- * TODO Contents: mit oder ohne Datenbank, auslesen von Typo3-Seiten, Erkennung der Sitemap, etc.
+ * TODO Contents: mit oder ohne Datenbank
  * TODO Formulare: erstellen von Formularen und annehmen der Daten
  * TODO Email: senden von Emails
  * TODO Framework: Vererbungsmöglichkeit, um Funktionen zu überschreiben
@@ -23,15 +26,17 @@ include 'users.inc';
  * TODO News: News/Blog schreiben
  * TODO Chat: WebChat oder Newsticker-Funktion
  * TODO Media: Musik, Video, Streaming und so
- * TODO Kalender: Allgemeiner Kalender mit Terminen, Erinerungen per Mail
+ * TODO Kalender: Allgemeiner Kalender mit Terminen, Erinnerungen per Mail
  * TODO Cronjob: Bezug auf eine Datei im Framework, damit er leicht erweiterbar ist
  * TODO Einfaches Newsletter-System mit eigener User-Datenbank,
  */
 class framework{
+    public $config;
     /**
      *
      */
     public function __construct(){
+        $this->config=configuration::loadConfig('framework');
         $this->images = new images();
         $this->users = new users();
         $this->contentpool = new contentpool();
--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include 'framework/framework.php';
 $framework = new framework();
 $content='';
 
-$framework->template->setTemplate('custom');
 $framework->template->setTemplateVariables(array('content',$content));
 $framework->template->setupScript('bootstrap');
 $framework->template->disableCaching();
